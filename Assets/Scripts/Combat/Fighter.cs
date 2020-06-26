ef7a8ce23e600e069eb076128a72f4b6617499a4
Wilder Medeiros <wilder.medeiros@outlook.com>
2020-06-26 19:41:30 -0300

    01 Primeiro commit com o andamento do curso, Triggering animation in code.

---
+++ Assets/Scripts/Combat/Fighter.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using RPG.Core;
+using RPG.Movement;
+using UnityEngine;
+
+namespace RPG.Combat
+{
+    public class Fighter : MonoBehaviour, IAction
+    {
+        [SerializeField] float distanteRange = 2f;
+        
+        Transform target;
+        
+        private void Update()
+        {
+            if (target == null) return;
+
+            CheckDistance();
+        }
+
+        private void CheckDistance()
+        {
+            if (!GetIsInRange())
+            {
+                GetComponent<Mover>().MoveTo(target.position);
+            }
+            else
+            {
+                GetComponent<Mover>().Cancel();
+                AttackBehaviour();
+            }
+        }
+
+        private void AttackBehaviour()
+        {
+            GetComponent<Animator>().SetTrigger("attack");
+        }
+
+        private bool GetIsInRange()
+        {
+            return Vector3.Distance(target.position, transform.position) <= distanteRange;
+        }
+
+        public void Attack(CombatTarget combatTarget)
+        {
+            target = combatTarget.transform;
+            GetComponent<ActionScheduler>().StartAction(this);
+        }
+
+        public void Cancel(){
+            target = null;
+        }
+
+        public void HitEvent() // Animation event reference
+        {
+        }
+    }
+}
---
+++ Assets/Scripts/Movement/Mover.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using RPG.Core;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.AI;
+
+namespace RPG.Movement
+{
+    public class Mover : MonoBehaviour, IAction
+    {
+        NavMeshAgent navMeshAgent;
+        Animator animator;
+
+        void Start()
+        {
+            navMeshAgent = GetComponent<NavMeshAgent>();
+            animator = GetComponent<Animator>();
+        }
+
+        void Update()
+        {
+            UpdateAnimator();
+        }
+
+        private void UpdateAnimator()
+        {
+            Vector3 velocity = navMeshAgent.velocity;
+            Vector3 localVelocity = transform.InverseTransformDirection(velocity).normalized;
+            float speed = localVelocity.z;
+
+            animator.SetFloat("forwardSpeed", speed);
+        }
+
+        public void StartMoveAction(Vector3 destination)
+        {
+            GetComponent<ActionScheduler>().StartAction(this);
+            MoveTo(destination);
+        }
+
+        public void MoveTo(Vector3 destination)
+        {
+            navMeshAgent.destination = destination;
+            navMeshAgent.isStopped = false;
+        }
+
+        public void Cancel()
+        {
+            navMeshAgent.isStopped = true;
+        }
+    }
+}
