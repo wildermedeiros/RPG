3a6846cbf731bbbd3240d8889949f399b91ed23e
Wilder Medeiros <wilder.medeiros@outlook.com>
2020-09-06 20:54:03 -0300

    11 - Tideupning the level

---
+++ Assets/Scripts/Core/Portal.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Portal : MonoBehaviour
+{    
+    [SerializeField] int sceneIndex = -1;
+
+    private void OnTriggerEnter(Collider other) 
+    {
+        if (other.CompareTag("Player"))
+        {
+            StartCoroutine(Transition());
+        }    
+    }
+
+    private IEnumerator Transition()
+    {
+        DontDestroyOnLoad(gameObject);
+        yield return SceneManager.LoadSceneAsync(sceneIndex); 
+        print("Carregueiss");
+        Destroy(gameObject);   
+    }
+}
