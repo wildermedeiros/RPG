da82632623e44328d4ebffd1993f95e011363c92
Wilder Medeiros <wilder.medeiros@outlook.com>
2020-09-11 18:57:41 -0300

    14 - Player Spawn Point

--- Assets/Scripts/Core/Portal.cs
+++ Assets/Scripts/Core/Portal.cs
@@ -1,11 +1,14 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.AI;
 
 public class Portal : MonoBehaviour
 {    
     [SerializeField] int sceneIndex = -1;
+    [SerializeField] Transform spawnPoint; 
 
     private void OnTriggerEnter(Collider other) 
     {
@@ -18,8 +21,30 @@ public class Portal : MonoBehaviour
     private IEnumerator Transition()
     {
         DontDestroyOnLoad(gameObject);
+        
         yield return SceneManager.LoadSceneAsync(sceneIndex); 
-        print("Carregueiss");
+
+        Portal otherPortal = GetOtherPortal();
+        UpdatePlayer(otherPortal);
+
         Destroy(gameObject);   
+    }
+
+    private void UpdatePlayer(Portal otherPortal)
+    {
+        GameObject player = GameObject.FindWithTag("Player");
+        player.GetComponent<NavMeshAgent>().Warp(otherPortal.spawnPoint.position);
+        player.transform.rotation = otherPortal.spawnPoint.rotation;
+    }
+
+    private Portal GetOtherPortal()
+    {
+        foreach(Portal portal in FindObjectsOfType<Portal>())
+        {
+            if (portal == this) { continue; }
+
+            return portal; 
+        }
+        return null; 
     }
 }
