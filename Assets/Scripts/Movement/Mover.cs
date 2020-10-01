fe71277d2f966d0dc882da9469f9f5a87efb307d
Wilder Medeiros <wilder.medeiros@outlook.com>
2020-10-01 17:18:15 -0300

    17 - Saving and loading the player position on the world

--- Assets/Scripts/Movement/Mover.cs
+++ Assets/Scripts/Movement/Mover.cs
@@ -5,10 +5,11 @@ using RPG.Core;
 using UnityEditor;
 using UnityEngine;
 using UnityEngine.AI;
+using RPG.Saving;
 
 namespace RPG.Movement
 {
-    public class Mover : MonoBehaviour, IAction
+    public class Mover : MonoBehaviour, IAction, ISaveable
     {
         NavMeshAgent navMeshAgent;
         Animator animator;
@@ -58,6 +59,18 @@ namespace RPG.Movement
         public void Cancel()
         {
             navMeshAgent.isStopped = true;
+        }
+
+        public object CaptureState()
+        {
+            return new SerializableVector3(transform.position);
+        }
+
+        public void RestoreState(object state)
+        {
+            SerializableVector3 position = (SerializableVector3)state;
+            //transform.position = 
+            GetComponent<NavMeshAgent>().Warp(position.ToVector());
         }
     }
 }
