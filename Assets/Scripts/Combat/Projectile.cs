08b7ed331b3664329e6ac26312910f9bb1f6f20b
Wilder Medeiros <wilder.medeiros@outlook.com>
2020-12-05 21:10:21 -0300

    32 - Destruindo as particulas depois do impacto e refinando o comportamento delas.

--- Assets/Scripts/Combat/Projectile.cs
+++ Assets/Scripts/Combat/Projectile.cs
@@ -7,64 +7,78 @@ using UnityEngine;
 // TODO IDEIAS 
 //              Sticky arrows 
 
-public class Projectile : MonoBehaviour
+namespace RPG.Combat
 {
-    [SerializeField] float speed = 5f;
-    [SerializeField] bool isHeatSeeker = false;
-    [SerializeField] GameObject hitEffect = null; 
-
-    float damage = 0;
-    Health target = null;
-
-    private void Start() 
+    public class Projectile : MonoBehaviour
     {
-        // colocando o lookat aq ele não terá comportamento de heatSeeker
-        transform.LookAt(GetAimLocation());
-    }
+        [SerializeField] float speed = 5f;
+        [SerializeField] bool isHeatSeeker = false;
+        [SerializeField] GameObject hitEffect = null;
+        [SerializeField] float maxLifeTime = 10f;
+        [SerializeField] GameObject[] destroyOnHit = null;
+        [SerializeField] float lifeTimeAfterImpact = 2f;
 
-    void Update()
-    {
-        // colocando o lookat aqui ele terá uma comportamento de heatseeker 
-        if (isHeatSeeker && !target.IsDead())
+        float damage = 0;
+        Health target = null;
+
+        private void Start()
         {
+            // colocando o lookat aq ele não terá comportamento de heatSeeker
             transform.LookAt(GetAimLocation());
         }
-        transform.Translate(Vector3.forward * speed *Time.deltaTime);
-    }
 
-    public void SetTarget(Health target, float damage)
-    {
-        this.target = target;
-        this.damage = damage;
-    }
-
-    private Vector3 GetAimLocation()
-    {
-        CapsuleCollider targetCollider = target.GetComponent<CapsuleCollider>();
-        if (targetCollider == null)
+        void Update()
         {
-            return target.transform.position;
-        }
-        return target.transform.position + Vector3.up * targetCollider.height / 2;  
-    }
-
-    private void OnTriggerEnter(Collider other) 
-    {
-        // posso colocar sem especificar o target, assim ela acerta quem estiver na frente 
-        if (other.GetComponent<Health>() != target) { return; }
-        
-        if(target.IsDead()) { return; }
-
-        target.TakeDamage(damage);
-        
-        if (hitEffect != null)
-        {
-            Instantiate(hitEffect, GetAimLocation(), transform.rotation);
-                                                    //Quaternion.LookRotation(target.transform.forward, target.transform.up)
+            // colocando o lookat aqui ele terá uma comportamento de heatseeker 
+            if (isHeatSeeker && !target.IsDead())
+            {
+                transform.LookAt(GetAimLocation());
+            }
+            transform.Translate(Vector3.forward * speed * Time.deltaTime);
         }
 
-        Destroy(gameObject);
-        // destruir depois de um tempo, ou se colidir com alguma coisa  
+        public void SetTarget(Health target, float damage)
+        {
+            this.target = target;
+            this.damage = damage;
+
+            Destroy(gameObject, maxLifeTime);
+        }
+
+        private Vector3 GetAimLocation()
+        {
+            CapsuleCollider targetCollider = target.GetComponent<CapsuleCollider>();
+            if (targetCollider == null)
+            {
+                return target.transform.position;
+            }
+            return target.transform.position + Vector3.up * targetCollider.height / 2;
+        }
+
+        private void OnTriggerEnter(Collider other)
+        {
+            // posso colocar sem especificar o target, assim ela acerta quem estiver na frente 
+            if (other.GetComponent<Health>() != target) { return; }
+            if (target.IsDead()) { return; }
+            target.TakeDamage(damage);
+
+            speed = 0;
+
+            if (hitEffect != null)
+            {
+                Instantiate(hitEffect, GetAimLocation(), transform.rotation);
+                //Quaternion.LookRotation(target.transform.forward, target.transform.up)
+            }
+
+            foreach (var toDestroy in destroyOnHit)
+            {
+                Destroy(toDestroy);
+            }
+
+            Destroy(gameObject, lifeTimeAfterImpact);
+            // destruir depois de um tempo, ou se colidir com alguma coisa  
+        }
+
     }
 
 }
