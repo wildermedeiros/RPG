5846320aab02cffc0849b4bc3fc3f3e2a816715f
Wilder Medeiros <wildersmedeiros@gmail.com>
2022-01-12 02:58:57 -0300

    55 - Generic Raycastable Components

--- Assets/Scripts/Control/PlayerController.cs
+++ Assets/Scripts/Control/PlayerController.cs
@@ -36,14 +36,14 @@ namespace RPG.Control
 
         void Update()
         {
-            if(InteractWithUI()) return;
+            if(InteractWithUI()) { return; }
             if(health.IsDead()) 
             {
                 SetCursor(CursorType.None);
                 return; 
             }
             
-            if (InteractWithCombat()) { return; }
+            if (InteractWithComponent()) { return; }
             if (InteractWithMovement()) { return; }
 
             SetCursor(CursorType.None);
@@ -59,22 +59,20 @@ namespace RPG.Control
             return false; 
         }
 
-        private bool InteractWithCombat()
+        private bool InteractWithComponent()
         {
             RaycastHit[] hits = Physics.RaycastAll(GetMouseRay());
             foreach (RaycastHit hit in hits)
             {
-                CombatTarget target = hit.transform.GetComponent<CombatTarget>();
-                if(target == null) { continue; }
-
-                if (!GetComponent<Fighter>().CanAttack(target.gameObject)) { continue; }
-                
-                if (Input.GetMouseButton(0))
+                IRaycastable[] raycastables = hit.transform.GetComponents<IRaycastable>();
+                foreach (IRaycastable raycastable in raycastables)
                 {
-                    GetComponent<Fighter>().Attack(target.gameObject);
+                    if (raycastable.HandleRaycast(this))
+                    {
+                        SetCursor(CursorType.Combat);
+                        return true;
+                    }
                 }
-                SetCursor(CursorType.Combat);
-                return true;
             }
             return false;
         }
